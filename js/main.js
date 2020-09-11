01f9783011f04dd72817cb8ef88d2fa6d9a1b1f3
Adam Nelson <adam.nelson@teradata.com>
2020-09-11 14:47:11 -0400

    Add basic routing.

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var location = window.location.pathname.split('/')[2]; // Set to 1 for local development
+var embedSource = document.getElementById("prototype").src;
+
+console.log("location: " + location);
+
+if (location) {
+    switch (location) {
+        case "home":
+            page = "20%253A2360";
+            break;
+        case "changelog":
+            page = "65%253A14";
+            break;
+        case "guides":
+            page = "218%3A1466";
+            break;
+        case "basics":
+            page = "218%3A1332";
+            break;
+        case "components":
+            page = "218%3A600";
+            break;
+        case "features":
+            page = "218%3A1600";
+            break;
+        case "templates":
+            page = "218%3A1734";
+            break;
+        default:
+            page = "20%253A2360";
+    }
+
+    // Swap out iframe embed id to navigate straight to the requested page
+    var newSource = embedSource.replace("20%253A2360", page);
+    document.getElementById("prototype").src = newSource;
+}
